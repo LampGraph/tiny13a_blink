6d5c01307d73e9b8d38c3fc0c626c538e12fbc14
LampGraph <LampGraph@gmail.com>
2014-03-17 00:08:04 +0800

    new
    Signed-off-by: LampGraph <LampGraph@gmail.com>

---
+++ tiny13a_blink/tiny13a_blink.cpp
@@ -0,0 +1,27 @@
+/*
+ * tiny13a_blink.cpp
+ *
+ * Created: 15.03.2014 20:42:52
+ *  Author: Vyacheslav
+ */ 
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define LED_PIN PB4
+
+int main(void) {
+
+	// Configure LED_PIN (PB4) as output
+	DDRB |= (1 << LED_PIN);
+
+	while (1) {
+		PORTB |= (1 << LED_PIN);
+		_delay_ms(500);
+		PORTB &= ~(1 << LED_PIN);
+		_delay_ms(500);
+	}
+
+	return 0;
+}
